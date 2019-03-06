73b8700a49fb4de60a224afe654a703f5780949e
Rafael Carvalho <crcarvalho@outlook.com>
2019-03-06 17:46:54 -0300

    primeiro commit

---
+++ src/main/java/br/com/devmedia/jerseyrest/model/domain/Produto.java
@@ -0,0 +1,41 @@
+package br.com.devmedia.jerseyrest.model.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	private int quantidade;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	@Override
+	public String toString() {
+		return "Produto [id=" + id + ", nome=" + nome + ", quantidade=" + quantidade + "]";
+	}
+	
+}
